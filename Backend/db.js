72a323323532274baf68b1f25666054eeef2de7b
ygngzln <ygngzln@gmail.com>
2022-06-11 07:57:30 -0700

    Deployment 6

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config({ path: 'ENV_FILENAME' });
+const connectionString = process.env.MONGOBLOGS
 
 const connectDB = async () => {
     try {
-        const fig = await mongoose.connect(process.env.MONGOBLOGS)
+        const fig = await mongoose.connect(`${connectionString}`,{
+            useUnifiedTopology:true,
+            useNewUrlParser: true
+        })
         console.log(`MongoDB has been connnected at ${fig.connection.host}`)
     } catch (e){
         console.log(e)
